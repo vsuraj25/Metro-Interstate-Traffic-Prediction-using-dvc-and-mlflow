023ebac858f5d0382a776b37df36acc1ff36cb6e
vsuraj25 <55409076+vsuraj25@users.noreply.github.com>
2022-12-19 12:32:19 +0530

    Stage 1 complete, added logging and custom exception

---
+++ src/get_data_from_database.py
@@ -0,0 +1,76 @@
+from pymongo import MongoClient
+import pandas as pd
+import argparse
+import sys
+from util.util import read_yaml
+from logger import logging
+from exception import Project_Exception
+
+def upload_get_data(param_config_path):
+    try:
+        logging.info(f"{'-'*30} Loading the data from database {'-'*30}")
+        logging.info("Reading required parameters.")
+
+        param_config = read_yaml(param_config_path)
+        
+        connection_str = param_config['get_data']['connection_string']
+        database = param_config['get_data']['database_name']
+        source_data_path = param_config['data_source']['source_data']
+        table = param_config['get_data']['collection_name']
+
+        logging.info("Connecting to MongoDB client.")
+        client = MongoClient(connection_str)
+        # Creating a Database with specified name
+        logging.info("Getting Database.")
+        db =  client.get_database(database)
+
+        logging.info("Checking if updated records exists.")
+
+        if table in db.list_collection_names():
+            logging.info("Record already present.")
+            records = db.get_collection(table)
+            logging.info("Extracting data from database.")
+            all_records = records.find()
+            logging.info("Data Extracted Successfully.")
+            # Converting Curser object into list
+            cursor_list = list(all_records)
+
+            logging.info("Converting data into dataframe.")
+            data = pd.DataFrame(cursor_list).drop('_id', axis=1)
+            logging.info("Dataframe ready.")
+
+            return data
+
+        else:
+            logging.info("No Records present.")
+            logging.info("Loading the source data into database.")
+            records = db.traffic_records
+
+            df = pd.read_csv(source_data_path)
+
+            # Converting the dataframe into sictionary as MongoDB stores values as records/dictionary
+            df = df.to_dict(orient='records')
+
+            # Inserting the records into our mongoDB Database in collection 'fire_records'
+            records.insert_many(df)
+            logging.info("Data inserted into the database successfully.")
+
+            logging.info("Extracting data from database.")
+            all_records = records.find()
+            logging.info("Data Extracted Successfully.")
+            # Converting Curser object into list
+            cursor_list = list(all_records)
+
+            logging.info("Converting data into dataframe.")
+            data = pd.DataFrame(cursor_list).drop('_id', axis=1)
+            logging.info("Dataframe Ready.")
+
+            return data
+    except Exception as e:
+        raise Project_Exception(e, sys) from e
+
+if __name__ == '__main__':
+    args = argparse.ArgumentParser()
+    args.add_argument('--config', default='params.yaml')
+    parsed_args = args.parse_args()
+    data = upload_get_data(param_config_path=parsed_args.config)
---
+++ src/load_data.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import argparse
+import sys
+from util.util import read_yaml
+from get_data_from_database import upload_get_data
+from logger import logging
+from exception import Project_Exception
+
+def load_data(config_path):
+    try:
+        config = read_yaml(config_path)
+        df = upload_get_data(config_path)
+        raw_data_path = config['load_data']['raw_dataset_csv']
+
+        logging.info(f'Saving the data as csv at {raw_data_path}')
+        df.to_csv(raw_data_path, sep=',', index= False)
+        logging.info(f'Data saved at {raw_data_path}')
+    except Exception as e:
+        raise Project_Exception(e,sys) from e
+
+
+if __name__ == '__main__':
+    args = argparse.ArgumentParser()
+    args.add_argument('--config', default='params.yaml')
+    parsed_args = args.parse_args()
+    load_data(config_path = parsed_args.config)
+    
