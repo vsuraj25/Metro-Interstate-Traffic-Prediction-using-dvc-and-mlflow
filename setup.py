0de1517ef3cb495a1c97d0be524e34622c1765b4
vsuraj25 <55409076+vsuraj25@users.noreply.github.com>
2022-12-20 17:28:49 +0530

    tox.ini and setup.py added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='traffic_prediction',
+    version = "0.0.1",
+    description= "Metro Interstate Traffic Prediction",
+    author='vsuraj25',
+    packages= find_packages(),
+    license= 'Apache License'
+)
