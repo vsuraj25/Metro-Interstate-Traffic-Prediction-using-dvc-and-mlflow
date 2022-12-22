625e93576a7256b9f47a030959db7e708ca1e689
vsuraj25 <55409076+vsuraj25@users.noreply.github.com>
2022-12-22 11:59:34 +0530

    Validated Prediction Web app

--- prediction_service/prediction.py
+++ prediction_service/prediction.py
@@ -1,32 +1,73 @@
 import yaml
 import json
+import os 
 import joblib
 import numpy as np
 
 param_path = 'params.yaml'
+schema_path = os.path.join('prediction_service', 'schema_in.json')
+
+
+# Exception classes
+
+class NotInRange(Exception):
+    def __init__(self, message = 'Values entered are not in range!'):
+        self.message = message
+        super().__init__(self.message)
+
+class NotInCol(Exception):
+    def __init__(self, message = 'Invalid columns!'):
+        self.message = message
+        super().__init__(self.message)
+
 
 def read_yaml(config_path):
     with open(config_path) as yaml_file:
         config = yaml.safe_load(yaml_file)
     return config
 
+def get_schema(schema_path = schema_path):
+    with open(schema_path) as json_file:
+        schema = json.load(json_file)
+    return schema
 
-def form_response(dict_req):
-    data = dict_req.values()
-    data = [list(map(float, data))]
-    response = predict(data)
-    return response
+def validate_input(dict_request):
 
-def api_response(request):
-    try:
-        data = np.array([list(request.json.values())])
+    def _validate_cols(col):
+        schema = get_schema()
+        actual_cols = schema.keys()
+        if col not in actual_cols:
+            raise NotInCol
+
+    def _validate_values(col, val):
+        schema = get_schema()
+        if not (schema[col]['min'] <= float(dict_request[col]) <= schema[col]['max']):
+            raise NotInRange
+
+    for col, val in dict_request.items():
+        _validate_cols(col)
+        _validate_values(col, val)
+
+    return True
+
+def form_response(dict_request):
+    if validate_input(dict_request):
+        data = dict_request.values()
+        data = [list(map(float, data))]
         response = predict(data)
-        response = {'response' : response}
         return response
+
+def api_response(dict_request):
+    try:
+        if validate_input(dict_request):
+            data = np.array([list(dict_request.json.values())])
+            response = predict(data)
+            response = {'response' : response}
+            return response
     except Exception as e:
         print(e)
-        error = {"error": "Something went wrong!! Try again"}
-        return error
+        response = {'the_expected_range': get_schema(), 'response': str(e)}
+        return response
 
 def predict(data):
     config = read_yaml(param_path)
@@ -34,7 +75,14 @@ def predict(data):
     model = joblib.load(prediction_model)
     prediction = model.predict(data).tolist()[0]
 
-    return prediction
+    try:
+        if 0 <= prediction <= 7280: 
+            return prediction
+        else:
+            raise NotInRange
+    except NotInRange:
+        return "Unexpected Result"
+
 
 
     
